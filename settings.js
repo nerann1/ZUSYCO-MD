dc44e55d84b79da2eb3aed580b9dc5ceb5ee0b08
nerann1 <163856257+nerann1@users.noreply.github.com>
2024-03-19 15:53:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,11 +16,11 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94773736172",
-  SUDO_NUMBERS : "94773736172",
+  OWNER_NUMBER: "94760243982",
+  SUDO_NUMBERS : "94760243982",
   OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
-  ZUSYCO_FILE : "ZUSYCO-QR-2.",
+  ZUSYCO_FILE : "ZUSYCO-QR-4.",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
     WELCOME: 'https://i.ibb.co/PzhpBV7/WELCOME-IMAGE-LOWQ.jpg',
